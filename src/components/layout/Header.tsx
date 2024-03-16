dbeb979a39922be7e1955d99518a468726eb6c7e
David Darx <david.darx@liip.ch>
2024-03-16 11:00:26 +0100

    feat(header): update link to home

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header>
       <h2 className='p-gutter lg:fixed lg:left-0 lg:top-0'>
-        <PageLink href='/'>
+        <PageLink href='/posts'>
           <span className='h3'>Dr. Hazbi Avdiji</span>
           <span className='block text-base'>Diversity | Post-growth | Design</span>
         </PageLink>
