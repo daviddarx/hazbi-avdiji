299b1cdef7de7352a0b9a1fa9958136a569af0d2
David Darx <david.darx@liip.ch>
2024-03-16 09:14:08 +0100

    feat(header): implement content

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,8 +3,11 @@ import PageLink from '@/components/ui/PageLink';
 export default function Header() {
   return (
     <header>
-      <h2 className='h1 p-gutter lg:fixed lg:left-0 lg:top-0'>
-        <PageLink href='/'>Logo</PageLink>
+      <h2 className='p-gutter lg:fixed lg:left-0 lg:top-0'>
+        <PageLink href='/'>
+          <span className='h3'>Dr. Hazbi Avdiji</span>
+          <span className='block text-base'>Diversity | Post-growth | Design</span>
+        </PageLink>
       </h2>
     </header>
   );
