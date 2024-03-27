b4238b6c1947e3c62decad7a500dfb45095ccff5
David Darx <david.darx@liip.ch>
2024-03-27 15:50:54 +0100

    style(header): implement layout

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,9 +6,15 @@ export default function Header() {
     <header>
       <h2 className='p-gutter lg:fixed lg:left-0 lg:top-0'>
         <PageLink href='/posts'>
-          <Logo />
+          <Logo className={'w-[120px]'} />
           <span className='sr-only'>Dr. Hazbi Avdiji</span>
-          <span className='font-text block text-base'>Diversity | Post-growth | Design</span>
+          <span className='font-text mt-gutter-1/2 block text-sm leading-title'>
+            Diversity
+            <br />
+            Post-growth
+            <br />
+            Design
+          </span>
         </PageLink>
       </h2>
     </header>
