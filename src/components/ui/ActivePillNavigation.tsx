9a6f0c2c11d87cc9f472c3e318af65a56653cd75
David Darx <david.darx@liip.ch>
2024-04-27 14:24:24 +0200

    feat(active-pill-navigation): implement hover effect

--- src/components/ui/ActivePillNavigation.tsx
+++ src/components/ui/ActivePillNavigation.tsx
@@ -1,6 +1,22 @@
 import classNames from 'classnames';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 
+type dimensions = {
+  top: string | number;
+  left: string | number;
+  width: number;
+  height: number;
+  opacity: number;
+};
+
+const defaultDimensions = {
+  top: '50%',
+  left: '50%',
+  width: 0,
+  height: 0,
+  opacity: 0,
+};
+
 export default function ActivePillNavigation({
   children = [],
   title = '',
@@ -15,44 +31,38 @@ export default function ActivePillNavigation({
   const container = useRef<HTMLElement>(null);
   const [activeChild, setActiveChild] = useState<null | HTMLElement>(null);
 
-  const [activeDimensions, setActiveDimensions] = useState<{
-    top: string | number;
-    left: string | number;
-    width: number;
-    height: number;
-    opacity: number;
-  }>({
-    top: '50%',
-    left: '50%',
-    width: 0,
-    height: 0,
-    opacity: 0,
-  });
+  const [activeDimensions, setActiveDimensions] = useState<dimensions>(defaultDimensions);
+  const [hoverDimensions, setHoverDimensions] = useState<dimensions>(defaultDimensions);
+
+  const getItemDimensions = (item: HTMLElement, parent: HTMLElement) => {
+    return {
+      top: parent.offsetTop,
+      left: parent.offsetLeft - 4,
+      width: item.offsetWidth + 8,
+      height: item.offsetHeight,
+      opacity: 1,
+    };
+  };
+
+  const getDeactiveDimensions = (currentDimensions: dimensions) => {
+    return {
+      ...currentDimensions,
+      opacity: 0,
+    };
+  };
 
   const handleActiveItem = useCallback((item: HTMLElement | null | undefined) => {
     if (item) {
       const parent = item.parentNode as HTMLElement;
       if (parent) {
-        setActiveDimensions({
-          top: parent.offsetTop,
-          left: parent.offsetLeft - 4,
-          width: item.offsetWidth + 8,
-          height: item.offsetHeight,
-          opacity: 1,
+        setActiveDimensions(getItemDimensions(item, parent));
+        setHoverDimensions((currentDimensions) => {
+          return getDeactiveDimensions(currentDimensions);
         });
       }
     } else {
       setActiveDimensions((currentDimensions) => {
-        return {
-          top: '50%',
-          left:
-            typeof currentDimensions.left === 'number'
-              ? currentDimensions.left + currentDimensions.width * 0.5
-              : currentDimensions.left,
-          width: 0,
-          height: 0,
-          opacity: 0,
-        };
+        return getDeactiveDimensions(currentDimensions);
       });
     }
   }, []);
@@ -61,12 +71,38 @@ export default function ActivePillNavigation({
     handleActiveItem(activeChild);
   }, [activeChild, handleActiveItem]);
 
+  const handleMouseOver = useCallback((e: MouseEvent) => {
+    const target = e.target as HTMLElement;
+    if (target.hasAttribute('data-clickable')) {
+      const parent = target.parentNode as HTMLElement;
+      if (parent) {
+        setHoverDimensions(getItemDimensions(target, parent));
+      }
+    }
+  }, []);
+
+  const handleMouseOut = useCallback((e: MouseEvent) => {
+    setHoverDimensions((currentDimensions) => {
+      return getDeactiveDimensions(currentDimensions);
+    });
+  }, []);
+
   useEffect(() => {
+    const nav = container.current;
     window.addEventListener('resize', handleResize);
+    if (nav) {
+      nav.addEventListener('mouseover', handleMouseOver);
+      nav.addEventListener('mouseout', handleMouseOut);
+    }
+
     return () => {
       window.removeEventListener('resize', handleResize);
+      if (nav) {
+        nav.removeEventListener('mouseover', handleMouseOver);
+        nav.removeEventListener('mouseout', handleMouseOut);
+      }
     };
-  }, [handleResize]);
+  }, [handleResize, handleMouseOver, handleMouseOut]);
 
   useEffect(() => {
     const activeElement = container.current?.querySelector(
@@ -101,6 +137,12 @@ export default function ActivePillNavigation({
             'absolute -z-10 transform-gpu rounded-full border border-black bg-theme-next transition-all duration-500 ease-in-out-quart',
           )}
           style={activeDimensions}
+        ></div>
+        <div
+          className={classNames(
+            'absolute -z-20 transform-gpu rounded-full border border-black bg-theme-prev transition-all duration-300 ease-out-quart',
+          )}
+          style={hoverDimensions}
         ></div>
       </ul>
     </nav>
