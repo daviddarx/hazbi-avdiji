866f4bd8265a8bca1f0e7d8f67bd23423681c067
David Darx <david.darx@liip.ch>
2024-05-04 10:01:32 +0200

    feat(icon): fix react properties

--- src/components/ui/Icon.tsx
+++ src/components/ui/Icon.tsx
@@ -11,9 +11,9 @@ export default function Icon({ name, className }: { name: 'close'; className?: s
       {name === 'close' && (
         <path
           stroke='currentColor'
-          stroke-linecap='round'
-          stroke-linejoin='round'
-          stroke-width='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          strokeWidth='2'
           d='M18 6 6 18M6 6l12 12'
         />
       )}
