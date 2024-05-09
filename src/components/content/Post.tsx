d68bdc742ecc59b6899231b50733fa8a10e2aead
David Darx <david.darx@liip.ch>
2024-05-09 15:41:28 +0200

    feat(post): implement layout

--- src/components/content/Post.tsx
+++ src/components/content/Post.tsx
@@ -1,19 +1,36 @@
 import PageLink from '@/components/ui/PageLink';
 import type { Post } from '@/tina/types';
 import { postRoute } from '@/utils/tina';
+import { classNames } from 'tinacms';
 import { tinaField } from 'tinacms/dist/react';
 
 export default function PostList(props: { post: Post }) {
   return (
     <PageLink
       href={`${postRoute}/${props.post._sys.filename}`}
-      className='border-semi-transparent flex h-full flex-col justify-between gap-gutter rounded-cards p-gutter transition-colors hover:border-black hover:bg-theme-prev'
+      className={classNames(
+        'group relative h-full hashover:hover:z-20',
+        'before:bg-blurred before:border-semi-transparent before:pointer-events-none before:absolute before:-inset-80 before:scale-50 before:rounded-cards before:opacity-0',
+        'before:transition-all before:duration-100 before:ease-in-quart',
+        'hashover:hover:before:scale-100 hashover:hover:before:opacity-100 hashover:hover:before:duration-300 hashover:hover:before:ease-out-quart',
+      )}
     >
-      <h3 className='' data-tina-field={tinaField(props.post, 'title')}>
-        {props.post.title}
-      </h3>
-      <div className='sr-only flex gap-16'>
-        <span className='font-bold'>{props.post.category.title}</span>
+      <div
+        className={classNames(
+          'border-semi-transparent relative z-10 flex h-full flex-col items-start justify-between gap-gutter rounded-cards p-gutter transition-colors',
+          'hashover:hover:z-20 hashover:hover:border-black hashover:hover:bg-theme-prev',
+        )}
+      >
+        <h3 data-tina-field={tinaField(props.post, 'title')}>{props.post.title}</h3>
+        <div
+          className={classNames(
+            'tag absolute top-0 opacity-0 hashover:group-hover:-translate-y-1/2',
+            'transition-[opacity,transform] delay-[0s,200ms] duration-[200ms,200ms] ease-out-quart',
+            'hashover:group-hover:opacity-100 hashover:group-hover:delay-100 hashover:group-hover:duration-[300ms,300ms]',
+          )}
+        >
+          {props.post.category.title}
+        </div>
       </div>
     </PageLink>
   );
