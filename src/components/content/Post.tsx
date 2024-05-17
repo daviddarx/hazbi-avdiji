fa1e2d065dd4c015e5a5c992b9ac49f237d45aa8
David Darx <david.darx@liip.ch>
2024-05-17 08:54:29 +0200

    feat(post): fix focus state

--- src/components/content/Post.tsx
+++ src/components/content/Post.tsx
@@ -10,7 +10,7 @@ export default function PostList(props: { post: Post }) {
     <PageLink
       href={`${postRoute}/${props.post._sys.filename}`}
       className={classNames(
-        'group relative h-full hashover:hover:z-20',
+        'group relative block h-full rounded-cards hashover:hover:z-20',
         'before:bg-blurred before:border-light before:pointer-events-none before:absolute before:-inset-80 before:scale-50 before:rounded-cards before:opacity-0',
         'before:transition-all before:duration-100 before:ease-in-quart',
         'hashover:hover:before:scale-100 hashover:hover:before:opacity-100 hashover:hover:before:duration-300 hashover:hover:before:ease-out-quart',
@@ -23,14 +23,14 @@ export default function PostList(props: { post: Post }) {
           'hashover:group-hover:-translate-y-[40%] hashover:group-hover:opacity-100 hashover:group-hover:delay-100 hashover:group-hover:duration-[300ms,300ms]',
         )}
       />
-      <div
+      <span
         className={classNames(
           'border-light relative z-10 flex h-full flex-col items-start justify-between gap-gutter rounded-cards p-gutter transition-colors',
           'hashover:hover:bg-themed-prev hashover:hover:border-strong dark:hashover:hover:border-light',
         )}
       >
         <h3 data-tina-field={tinaField(props.post, 'title')}>{props.post.title}</h3>
-        <div
+        <span
           className={classNames(
             'tag absolute bottom-0 translate-y-full opacity-0',
             'transition-[opacity,transform] delay-[0s,200ms] duration-[200ms,200ms] ease-out-quart',
@@ -38,8 +38,8 @@ export default function PostList(props: { post: Post }) {
           )}
         >
           {props.post.category.title}
-        </div>
-      </div>
+        </span>
+      </span>
     </PageLink>
   );
 }
