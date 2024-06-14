732d4a6e62cb55ee206965e6633b072d4a6688c9
David Darx <david.darx@liip.ch>
2024-06-14 15:11:36 +0200

    fix(404): provide footer navigation

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,15 @@
 import Metas from '@/components/layout/Metas';
 import PageWrapper from '@/components/layout/PageWrapper';
 import t from '@/content/translations';
+import { FooteNavigationResult } from '@/types/';
 
-export default function Custom404() {
+export default function Custom404({
+  footerNavigationProps,
+}: {
+  footerNavigationProps: FooteNavigationResult;
+}) {
   return (
-    <PageWrapper>
+    <PageWrapper footerNavigationProps={footerNavigationProps}>
       <Metas title={t.errorPage.title} />
       <h1>{t.errorPage.title}</h1>
     </PageWrapper>
