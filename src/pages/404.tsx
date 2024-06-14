bc4f28694e11a4ea387a40fdcb0f0cae71f8bcc1
David Darx <david.darx@liip.ch>
2024-06-14 19:42:06 +0200

    fix(404): fix missing data

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,7 @@
 import Metas from '@/components/layout/Metas';
 import PageWrapper from '@/components/layout/PageWrapper';
 import t from '@/content/translations';
+import client from '@/tina/client';
 import { FooteNavigationResult } from '@/types/';
 
 export default function Custom404({
@@ -15,3 +16,16 @@ export default function Custom404({
     </PageWrapper>
   );
 }
+
+export const getStaticProps = async ({ params }: { params: { slug?: string[] } }) => {
+  const footerNavigationResult = await client.queries.footerNavigation({
+    relativePath: 'footer-navigation.md',
+  });
+
+  return {
+    props: {
+      footerNavigationProps: { ...footerNavigationResult },
+    },
+    revalidate: 10,
+  };
+};
