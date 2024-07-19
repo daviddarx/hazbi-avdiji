a51888a3827f5c901cc60a42ca140af241fa91d7
David Darx <david.darx@liip.ch>
2024-07-19 09:52:28 +0200

    feat(home-content): add gsap animation

--- src/components/content/HomeContent.tsx
+++ src/components/content/HomeContent.tsx
@@ -1,8 +1,37 @@
 import CustomMarkdown from '@/components/ui/CustomMarkdown';
 import { PageBlocksHomeContent } from '@/tina/types';
+import { useGSAP } from '@gsap/react';
+import { gsap } from 'gsap';
+import { useRef } from 'react';
 import { tinaField } from 'tinacms/dist/react';
 
-export default function TextContent({ content }: { content: PageBlocksHomeContent }) {
+gsap.registerPlugin(useGSAP);
+
+export default function HomeContent({ content }: { content: PageBlocksHomeContent }) {
+  const container = useRef<HTMLDivElement | null>(null);
+
+  useGSAP(
+    () => {
+      if (container.current) {
+        const divs = container.current.querySelectorAll('div');
+        divs.forEach((div, i) => {
+          gsap.set(div, { rotation: i * 2 });
+          gsap.to(div, {
+            rotation: 180,
+            borderRadius: 200,
+            delay: i * 0.1,
+            duration: 10,
+            scale: 1 - i * 0.05,
+            ease: 'back.out(2)',
+            repeat: -1,
+            yoyo: true,
+          });
+        });
+      }
+    },
+    { scope: container },
+  );
+
   return (
     <section className='grid-layout min-h-full'>
       <div className='grid-item-right relative z-10'>
@@ -13,7 +42,14 @@ export default function TextContent({ content }: { content: PageBlocksHomeConten
           <CustomMarkdown content={content.introduction} />
         </div>
       </div>
-      <div className='home-visual'>
+      <div className='home-visual' ref={container}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
         <div></div>
         <div></div>
         <div></div>
