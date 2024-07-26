496ff266b4115b31fc27a6ff99348bbe074be7f6
David Darx <david.darx@liip.ch>
2024-07-26 15:37:35 +0200

    style(footer): finalize responsive

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,10 +9,9 @@ export default function Footer(props: FooteNavigationResult) {
 
   return (
     <footer className='grid-layout mt-auto '>
-      <div className='grid-item-full border-light flex justify-between border-t pb-spacer-48 pt-spacer-48 text-base lg:pb-72'>
-        <div>© {date.getFullYear()} Hazbi Avdiji</div>
+      <div className='grid-item-full border-light flex flex-wrap justify-between gap-20 border-t py-gutter text-sm md:py-spacer-48 md:text-base lg:pb-72'>
         {footerNavigation.links && (
-          <nav className='flex gap-spacer-48 lg:gap-40'>
+          <nav className='flex gap-4 max-md:flex-col md:gap-40'>
             <h2 className='sr-only'>Footer Navigation</h2>
             {footerNavigation.links.map((link) => (
               <PageLink
@@ -26,6 +25,7 @@ export default function Footer(props: FooteNavigationResult) {
             ))}
           </nav>
         )}
+        <div>© {date.getFullYear()} Hazbi Avdiji</div>
       </div>
     </footer>
   );
