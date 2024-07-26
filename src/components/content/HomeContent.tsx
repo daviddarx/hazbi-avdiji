990ae2a618cf0ba1a80895c52c7e3b8f8f9b4f08
David Darx <david.darx@liip.ch>
2024-07-26 15:30:02 +0200

    style(home-content): fix mobile layout

--- src/components/content/HomeContent.tsx
+++ src/components/content/HomeContent.tsx
@@ -33,12 +33,12 @@ export default function HomeContent({ content }: { content: PageBlocksHomeConten
   );
 
   return (
-    <section className='grid-layout min-h-full'>
+    <section className='grid-layout'>
       <div className='grid-item-right relative z-10'>
         <h1 className='mb-spacer-32 text-pretty' data-tina-field={tinaField(content, 'title')}>
           {content.title}
         </h1>
-        <div className='ml-1/4' data-tina-field={tinaField(content, 'introduction')}>
+        <div className='sm:ml-1/4' data-tina-field={tinaField(content, 'introduction')}>
           <CustomMarkdown content={content.introduction} />
         </div>
       </div>
